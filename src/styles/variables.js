47db11c2817266ea906161c6429c8dc8ebfd9140
atmozki <dennisjojo052001@gmail.com>
2023-03-24 18:29:40 +0530

    color change

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -2,8 +2,8 @@ import { css } from 'styled-components';
 
 const variables = css`
   :root {
-    --dark-navy: #020c1b;
-    --navy: #0a192f;
+    --dark-navy: #0D0208;
+    --navy: #008F11;
     --light-navy: #112240;
     --lightest-navy: #233554;
     --navy-shadow: rgba(2, 12, 27, 0.7);
@@ -12,7 +12,7 @@ const variables = css`
     --light-slate: #a8b2d1;
     --lightest-slate: #ccd6f6;
     --white: #e6f1ff;
-    --green: #64ffda;
+    --green: #00FF41;
     --green-tint: rgba(100, 255, 218, 0.1);
     --pink: #f57dff;
     --blue: #57cbff;
